d3cbe457a6a4fd2a8a52a8b414e46f4de85a2b16
Hasan Moussawi <hasanmoussawi@hotmail.com>
2022-01-23 20:49:18 +0200

    cart component

--- client/src/components/Filter/Filter.js
+++ client/src/components/Filter/Filter.js
@@ -5,7 +5,7 @@ function Filter(props) {
     return (
         <div className="filter-wrapper">
             <h2 className="filter-title"> Filter </h2>
-            <div className="num-of-products">Number of Products 4 </div>
+            <div className="num-of-products">Number of Products {props.productQuantity} </div>
             <div className="filter-by-size">
                 <span>Filter</span>
                 <select value={props.sizesort} className="filter-select" onChange={props.handleSizeSort}>
